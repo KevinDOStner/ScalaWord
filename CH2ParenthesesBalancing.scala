b36213fa23503323dbd52d1025d432f31a4459d2
Noah Brace <56211149+KevinDOStner@users.noreply.github.com>
2019-10-06 17:51:50 -0500

    Update CH2ParenthesesBalancing.scala
    Updated code to incorporate CH4 Pattern Matching concepts.

--- CH2ParenthesesBalancing.scala
+++ CH2ParenthesesBalancing.scala
@@ -5,21 +5,17 @@
    * from Chapter 2 of my Coursera Scala class, regarding Currying concepts
    * author @noodles
    */
-   
-  def balance(chars: List[Char]): Boolean = {
-    def f(chars: List[Char], numOpens: Int): Boolean = {
-      if (chars.isEmpty) {
-        numOpens == 0
-      } else {
-        val h = chars.head
-        val n =
-          if (h == '(') numOpens + 1
-          else if (h == ')') numOpens - 1
-          else numOpens
-        if (n >= 0) f(chars.tail, n)
-        else false
+
+def balance(chars: List[Char]): Boolean = {
+    def f(chars: List[Char], num: Int): Boolean = {
+      (chars,num) match {
+        case (Nil,num) => return num == 0
+        case (c :: tail,num) =>
+          if(num < 0) return false
+          if(c == '(') return f(tail,num+1)
+          if(c == ')') return f(tail,num-1)
+          else return f(tail,num)
       }
     }
-
-    f(chars, 0)
+    f(chars,0)
   }
