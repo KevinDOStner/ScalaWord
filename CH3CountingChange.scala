a4632c24dce0162115fb7c5bc8d143ca41b8f955
Noah Brace <56211149+KevinDOStner@users.noreply.github.com>
2019-10-06 17:56:03 -0500

    Update CH3CountingChange.scala

--- CH3CountingChange.scala
+++ CH3CountingChange.scala
@@ -13,3 +13,28 @@ def countChange(money: Int, coins: List[Int]): Int = {
   if (money == 0) return 1
   countChange(money - coins.head, coins) + countChange(money, coins.tail)
 }
+
+
+/* ### Deprecated code:
+
+def countChange(amount: Int): Int = {
+  def firstDenomination(kindsOfCoins: Int): Int = {
+    kindsOfCoins match {
+      case 1 => 1
+      case 2 => 5
+      case 3 => 10
+      case 4 => 25
+      case 5 => 50
+    }
+  }
+
+  def cc(amount: Int, kindOfCoins: Int): Int = {
+    if (amount == 0) 1
+    else if (amount < 0 || kindOfCoins == 0) 0
+    else cc(amount, kindOfCoins - 1) + cc(amount - firstDenomination(kindOfCoins), kindOfCoins)
+  }
+
+  cc(amount, 5)
+}
+countChange(100) //should be 292
+*/
