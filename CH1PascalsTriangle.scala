db425d30e4f92cb24313ae076f2f238e247a7a0f
Noah Brace <56211149+KevinDOStner@users.noreply.github.com>
2019-10-06 17:50:31 -0500

    Update CH1PascalsTriangle.scala
    Updated to incorporate Pattern Matching concepts from CH4

--- CH1PascalsTriangle.scala
+++ CH1PascalsTriangle.scala
@@ -5,9 +5,18 @@ import common._
 * This is a solution to Chapter 1 Assignment from Pascals Triangle that uses recursion in Scala.
 * author @noodles
 */
-  def pascal(c: Int, r: Int): Int = {
-    if (c == 0 || c == r) 1
-    else pascal(c - 1, r - 1) + pascal(c, r - 1)
- }
+ def balance(chars: List[Char]): Boolean = {
+    def f(chars: List[Char], num: Int): Boolean = {
+      (chars,num) match {
+        case (Nil,num) => return num == 0
+        case (c :: tail,num) =>
+          if(num < 0) return false
+          if(c == '(') return f(tail,num+1)
+          if(c == ')') return f(tail,num-1)
+          else return f(tail,num)
+      }
+    }
+    f(chars,0)
+  }
 
  
