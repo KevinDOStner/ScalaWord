cca28395cff585b849687f2d88cbe52d68bf4f28
Noah Brace <56211149+KevinDOStner@users.noreply.github.com>
2019-10-06 17:54:30 -0500

    Updated Code to incorporate Pattern Matching concepts

--- CH3CountingChange.scala
+++ CH3CountingChange.scala
@@ -8,31 +8,8 @@ import common._
    * author @noodles
    * This is a recursive function that counts how many different ways change can be made with a given a list of coins. 
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-    def f(lastMaxCoin_total_coll: List[(Int, Int)], count: Int): Int = {
-      if (lastMaxCoin_total_coll.isEmpty) {
-        count
-      } else {
-        val b = ListBuffer[(Int, Int)]()
-        var newCount = count
-        for ((lastMaxCoin, total) <- lastMaxCoin_total_coll) {
-          if (total < money) {
-            for (c <- coins) {
-              if (c >= lastMaxCoin) {
-                val e = (c, total + c)
-                b += e
-              }
-            }
-          } else if (total == money) {
-            newCount += 1
-          }
-        }
-
-        f(b.toList, newCount)
-      }
-    }
-
-    val b = coins.map { c => (c, c) }
-    f(b, 0)
-  }
+def countChange(money: Int, coins: List[Int]): Int = {
+  if (coins.isEmpty || money < 0) return 0
+  if (money == 0) return 1
+  countChange(money - coins.head, coins) + countChange(money, coins.tail)
 }
